ff15371c4da7ebc150a7500e8a34e2e162ce06c3
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2026-01-29 18:55:39 +0200

    first akrolithos commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-center h-screen justify-center items-center flex">
+      <h1>Akrolithos</h1>
+    </main>
+  );
+}
